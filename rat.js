cc70a0501f643646523cf3f86115b1302853c3f1
alaa afifi <alaaahmedafifi@gmail.com>
2019-01-22 23:11:28 +0200

    #

--- rat.js
+++ rat.js
@@ -1,30 +1,50 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql');
 
-var promise = new Promise(function(resolve, reject){
-    var x = 5;
-    if(x==5){
-        console.log('x is: ' + x);
-        resolve(x);
-    }else{
-        console.log('error occured');
-        reject('not a number');
-    }
+var path = require('path');
+
+var fs = require('fs');
+var session = require('express-session');
+
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine('html', require('ejs').renderFile);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({secret: "Shh, its a secret!"}));
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "",
+    password: "",
+    database: "attendance_system"
 });
 
 app.get('/', function(req, res){
-    // promise.then(
-    //     function(x){
-    //         console.log('then executed');
-    //         res.send("hello from home " + x);
-    //     },
-    //     function(err){
-    //         console.log(err);
-    //     }
-    // );    
+    res.render('pages/signin.html');
 });
 
 app.listen(8080, function(req, res){
-    console.log('server is listening at port: ' + 8080);
+    var table = 'arrival';
+    var date = '2019-1-23';
+    var time = '11:03:00';
+    con.connect(function(err) {
+        console.log('connected to db');
+        if (err) {
+            console.log('failed to insert arrival 1');
+            throw err;
+        }
+        con.query("INSERT INTO ?? (employee_id, day, time) VALUE (?,?,?)", [table, 1, date, time], function (err, result, fields) {
+            if (err) {
+                console.log('failed to insert in ' + table);
+                throw err;
+            }
+        });
+    });
 })
--- server.js
+++ server.js
@@ -3,26 +3,35 @@ var app = express();
 
 var mysql = require('mysql');
 
+var path = require('path');
+
 var fs = require('fs');
 var session = require('express-session');
 
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-//control flow module
-var step = require('step');
-
 /*** end of importing ****/
 
-//declaring middlewares
-app.use(express.static('public'));
+/*************************/
+/*** global variables ***/
+
+var image64;
+
+/*** end of global variables ***/
+/*************************/
+
+// view engine setup
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "/public")));
 app.engine('html', require('ejs').renderFile);
 
+// body parser setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({secret: "Shh, its a secret!"}));
 
+// database connection setup
 var con = mysql.createConnection({
     host: "localhost",
     user: "",
@@ -30,43 +39,49 @@ var con = mysql.createConnection({
     database: "attendance_system"
 });
 
-//retrieve employee data from db
-function getEmployeeData(empID, callback){
+// recording date and time
+function getArrivalDateTime(employeeID){
+    //getting date
+    var datetime = new Date();
+    var year = datetime.getFullYear();
+    var month = datetime.getMonth()+1;
+    var day = datetime.getDate();
+    var arrivalDate = [year, month, day].join('-');
+    console.log(arrivalDate);
+
+    //getting time
+    var hours = datetime.getHours();
+    var minutes = datetime.getMinutes();
+    var seconds = datetime.getSeconds();
+    var arrivalTime = [hours, minutes, seconds].join(':');
+    console.log(arrivalTime);
+    insertArrival(employeeID, arrivalDate, arrivalTime);
+}
+
+// updating database
+function insertArrival(employeeID, arrivalDate, arrivalTime){
     con.connect(function(err) {
-        if (err) throw err;
+        if (err) {
+            console.log('failed to insert arrival 1');
+            throw err;
+        }
         console.log('connected to database');
-        con.query("SELECT * FROM employee where employee_id = " + empID, function (err, result, fields) {
-            if (err) throw err;
-            callback(result);
+        con.query("INSERT INTO arrival (employee_id, day, time) VALUE (?,?,?)", [employeeID, arrivalDate, arrivalTime], function (err, result, fields) {
+            if (err) {
+                console.log('failed to insert in arrival 2');
+                throw err;
+            }
         });
     });
 }
 
-app.get('/', function(req, res){
-    console.log('a client is connected...');
-    res.render('pages/index.html');
-});
-
-app.get('/userPage', function(req, res){
-    console.log('userPage requested');
-    res.render('pages/user/userPage.html');
-})
-
-app.get('/userPage/:employeeID', function(req, res){
-    console.log('employee id recieved as a param is: ' + req.params.employeeID);
-    res.send('hello employee with id:' + req.params.employeeID)
-});
-
-app.get('/test', function(req, res){
-    res.render('pages/test.html');
-});
-
+//write image to directory  
 var writeImagePromise = function(req){
     return new Promise(function(resolve, reject){
         console.log('body: ' + JSON.stringify(req.body.content));
 
         //base64 recieved without meta info
-        var image64 = JSON.stringify(req.body.content);
+        image64 = JSON.stringify(req.body.content);
 
         //writing image to directory
         var path = 'D:\\attendance system\\shared\\test.png';
@@ -78,6 +93,7 @@ var writeImagePromise = function(req){
     });
 }
 
+// checking if file exists
 var checkFilePromise = function(){
     return new Promise(function(resolve, reject){
         const timeout = setInterval(function(){
@@ -91,20 +107,98 @@ var checkFilePromise = function(){
                 clearInterval(timeout);
                 resolve(file);
             }else{
-                console.log('still looking...');
+                console.log('still waiting...');
             }
-        }, 5000);
+        }, 2000);
     });
 }
 
+// reading contents of id.txt
 var getFileContentPromise = function(file){
     return new Promise(function(resolve, reject){
+        // reading employeeid
         var content = fs.readFileSync(file);
         var employeeID = content;
+
+        // recording arrival time
+        getArrivalDateTime(employeeID);
         resolve(employeeID);
     });
 }
 
+// getting employee data from db
+var getEmployeeDataPromise = function(employeeID){
+    return new Promise(function(resolve, reject){
+        // con.connect(function(err) {
+        //     if (err) throw err;
+        //     console.log('connected to database');
+            con.query("SELECT * FROM employee where employee_id = " + employeeID, function (err, result, fields) {
+                if (err) throw err;
+                resolve(result);
+            });
+        // });
+    });
+}
+
+// checking if id and password matches db
+//emploeeid is in function scope
+var employeeSigninPromise = function (employeeID, password){
+    return new Promise(function(resolve, reject){
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log('connected to database');
+            sql = "SELECT * FROM employee where employee_id = ? and password = ?";
+            con.query(sql, [employeeID, password], function (err, result, fields) {
+                if (err) throw err;
+                if(result.length == 1){
+                    // record attendance
+                    getArrivalDateTime(employeeID);
+                    resolve(result);
+                }
+            });
+        });
+    });
+}
+
+app.get('/', function(req, res){
+    console.log('a client is connected...');
+    res.render('pages/index.html');
+});
+
+// GET signin
+app.get('/signin', function(req, res){
+    res.render('pages/signin.html');
+});
+
+// POST signin
+app.post('/signin', function(req, res){
+    var employeeID = req.body.employeeID;
+    var password = req.body.password;
+    employeeSigninPromise(employeeID, password).then(function(result){
+        res.render('pages/userPage.html', {data:result[0]});
+    })
+});
+
+// GET userPage
+app.get('/userPage/:employeeID', function(req, res){
+    getEmployeeDataPromise(req.params.employeeID).then(function(result){
+        console.log(result);
+        res.render('pages/userPage.html', {data:result[0]});
+    })   
+});
+
+// testing
+app.get('/userPage', function(req, res){
+    console.log('userPage requested');
+    res.render('pages/user/userPage.html');
+});
+
+// testing page
+app.get('/test', function(req, res){
+    res.render('pages/test.html');
+});
+
+// POST request -> uploading image
 app.post('/upload', function(req, res){
     writeImagePromise(req).then(function(){
         return checkFilePromise();
@@ -114,6 +208,22 @@ app.post('/upload', function(req, res){
         res.status(200).send({body:req.body, url:'http://localhost:8080/userPage/' + result});
     })
 });
+
+// GET departure request
+app.get('/departure', function(req, res){
+    res.render('pages/departure.html');
+})
+
+// POST departure request
+app.post('/departure', function(req, res){
+    writeImagePromise(req).then(function(){
+        return checkFilePromise();
+    }).then(function(result){
+        return getFileContentPromise(result);
+    }).then(function(result){
+        res.status(200).send({body:req.body, url:'http://localhost:8080/userPage/' + result});
+    })
+})
 
 
 app.listen(8080, function(){
